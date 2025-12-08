914a1bd750d672bd5aa6239e507c9e2db5937fad
coongames8 <coongames8@gmail.com>
2025-12-08 19:44:15 +0300

    Update AppHelmet.jsx

--- src/components/AppHelmet.jsx
+++ src/components/AppHelmet.jsx
@@ -1,3 +1,4 @@
+// AppHelmet.jsx
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
@@ -5,25 +6,31 @@ export default function AppHelmet({ title, location }) {
   const canonicalUrl = `${location.origin}${location.pathname}`;
 
   return (
-		<Helmet>
-			<meta charSet="utf-8" />
-			<title>
-				{title} | Goal Kings - Fixed VIP Football Tips, Insights and News
-			</title>
-			<link rel="canonical" href={canonicalUrl} />
-			<meta
-				name="description"
-				content="Get all the latest ✓Fixed VIP Tips ✓Football Predictions, ✓Latest Football Betting Odds and livescores, results & fixtures for all leagues and competitions, including the Premier League, Championship and across the world."
-			/>
-			{/* Open Graph tags for social media */}
-			<meta property="og:title" content={title} />
-			<meta
-				property="og:description"
-				content="Get all the latest ✓Fixed VIP Tips ✓Football Predictions, ✓Latest Football Betting Odds and livescores, results & fixtures for all leagues and competitions, including the Premier League, Championship and across the world."
-			/>
-			<meta property="og:url" content={canonicalUrl} />
-			<meta property="og:type" content="website" />
-			<meta property="og:image" content="/logo512.png" />
-		</Helmet>
-	);
+    <Helmet>
+      <meta charSet="utf-8" />
+
+      <title>
+        {title} | Goal Kings - Football Insights, Analysis and News
+      </title>
+
+      <link rel="canonical" href={canonicalUrl} />
+
+      <meta
+        name="description"
+        content="Get the latest football insights, match analysis, live scores, results and fixtures for all major leagues and competitions worldwide, including the Premier League, Championship and more."
+      />
+
+      {/* Open Graph tags for social media */}
+      <meta property="og:title" content={`${title} | Goal Kings`} />
+
+      <meta
+        property="og:description"
+        content="Access up-to-date football insights, match analysis, live scores, results and fixtures for leagues and competitions across the world."
+      />
+
+      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/logo512.png" />
+    </Helmet>
+  );
 }
